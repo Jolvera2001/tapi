60148dba34db95b7ce1fe5b0da685921d815db2a
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-27 22:02:29 -0600

    got basic layout, figuring out logic

--- src/app.rs
+++ src/app.rs
@@ -10,39 +10,75 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
+
 #[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+struct RequestArgs<'a> {
+    url: &'a str,
 }
+
+// let greet = move |e: SubmitEvent| {
+//     e.prevent_default();
+//     spawn_local_scoped(async move {
+//         // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//         let args = serde_wasm_bindgen::to_value(&GreetArgs {
+// 			name: &name.get_clone()
+// 		})
+// 		.unwrap();
+//         let new_msg = invoke("greet", args).await;
+//         greet_msg.set(new_msg.as_string().unwrap());
+//     })
+// };
 
 #[component]
 pub fn App() -> View {
-    let count = create_signal(0);
+    view! {
+        div(class="w-screen h-screen") {
+            RequestComponent()
+        }
+    }
+}
 
-    // let greet = move |e: SubmitEvent| {
-    //     e.prevent_default();
-    //     spawn_local_scoped(async move {
-    //         // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    //         let args = serde_wasm_bindgen::to_value(&GreetArgs {
-	// 			name: &name.get_clone()
-	// 		})
-	// 		.unwrap();
-    //         let new_msg = invoke("greet", args).await;
-    //         greet_msg.set(new_msg.as_string().unwrap());
-    //     })
-    // };
+#[component]
+fn RequestComponent() -> View {
+    let request_value = create_signal(String::new());
+    let request_result = create_signal(String::new());
+    
+    let handle_submit = move |_| {
+        let url = request_value.get_clone();
+        spawn_local_scoped(async move {
+            if let Ok(args) = serde_wasm_bindgen::to_value(&RequestArgs {
+                url: &url
+            }) {
+                match invoke("make_request", args).await {
+                    Ok(response) => {
+
+                    },
+                    Err(e) => {
+                        request_result.set(format!("Error: {}", e));
+                    }
+                }
+            }
+        });
+    };
 
     view! {
-        div(class="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-5") {
-            div(class="p-6 bg-white rounded-lg shadow-lg") {
-                h1(class="text-2xl font-bold text-gray-800") { "Hello Tailwind!" }
-                h2(class="text-xl font-thin text-gray-700") { (count) }
+        div(class="flex flex-col p-2 space-y-2") {
+            div(class=" flex flex-row space-x-2") {
+                input(
+                    "type"="text",
+                    bind:value=request_value,
+                    placeholder="URL",
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                )
+                button(
+                    on:click=handle_submit,
+                    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                ) {
+                    "Send"
+                }
             }
-            div(class="flex flex-row space-x-5") {
-                button(class="p-6 text-md rounded-md bg-gray-700",
-                        on:click=move |_| count.set(count.get() + 1)) { "+" }
-                button(class="p-6 text-md rounded-md bg-gray-700",
-                        on:click=move |_| count.set(count.get() - 1)) { "-" }
+            div {
+                p(bind:value=request_result)
             }
         }
     }
