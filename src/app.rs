18d8dc1f9b2854b0852ad90b89cfa347ed8bcf40
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-27 22:19:32 -0600

    cleaned up imports

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 use sycamore::futures::spawn_local_scoped;
 use sycamore::prelude::*;
-use sycamore::web::events::SubmitEvent;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
