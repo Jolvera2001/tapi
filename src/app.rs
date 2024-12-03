f16ebd5fba37e913fe9b717437a53526adc8779a
Jolvera2001 <jolvera6@stedwards.edu>
2024-12-03 12:19:49 -0600

    got params list working

--- src/app.rs
+++ src/app.rs
@@ -9,15 +9,7 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
-
-#[derive(Serialize, Deserialize)]
-struct RequestArgs<'a> {
-    url: &'a str,
-}
-
-
 // we don't need to have lifetimes because we will own this data here
-
 // lifetimes -> data being sent as it will be gone after the command is invoked
 // owned <- data recieved from command
 #[derive(Serialize, Deserialize)]
@@ -41,6 +33,12 @@ pub fn App() -> View {
     }
 }
 
+#[derive(Clone, PartialEq)]
+struct Param {
+    key: String,
+    val: String,
+}
+
 #[component]
 fn RequestComponent() -> View {
     // data signals
@@ -49,6 +47,11 @@ fn RequestComponent() -> View {
     let request_method = create_signal(String::from("GET"));
     let status_code = create_signal(0);
     let body_content = create_signal(String::new());
+
+    // param tab
+    let params = create_signal(Vec::<Param>::new());
+    let new_key = create_signal(String::new());
+    let new_value = create_signal(String::new());
 
     // conditional signals
     let result_show = create_signal(true);
@@ -125,6 +128,8 @@ fn RequestComponent() -> View {
                     "Send"
                 }
             }
+
+            // tabs
             div(class="w-full mx-auto p-4") {
                 div(class="flex flex-col") {
                     div(class="flex border-b") {
@@ -158,7 +163,64 @@ fn RequestComponent() -> View {
                     div(class="my-4") {
                         (match active_tab.get() {
                             0 => view! {
-                                div { "Params view!" }
+                                    div(class="flex flex-col space-y-2") { 
+                                        div(class="flex flex-row space-x-2") {
+                                            input(
+                                                "type"="text",
+                                                bind:value=new_key,
+                                                placeholder="Parameter key",
+                                                class="border rounded p-2"
+                                            )
+                                            input(
+                                                "type"="text",
+                                                bind:value=new_value,
+                                                placeholder="Parameter key",
+                                                class="border rounded p-2"
+                                            )
+                                            button(
+                                                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow",
+                                                on:click=move |_| {
+                                                    let mut current = params.get_clone();
+                                                    current.push(Param {
+                                                        key: new_key.get_clone(),
+                                                        val: new_value.get_clone()
+                                                    });
+                                                    params.set(current);
+                                                    // Clear the inputs after adding
+                                                    new_key.set(String::new());
+                                                    new_value.set(String::new());
+                                                }
+                                            ) {
+                                                "Add Parameter"
+                                            }
+                                        }
+                                        Indexed(
+                                            list=params,
+                                            view=move |param| {
+                                                let param = param.clone();
+                                                let key_to_remove = param.key.clone();
+                                                view! {
+                                                    div(class="flex flex-row items-center justify-between bg-gray-50 rounded-lg p-2 hover:bg-gray-100 group") {
+                                                        div(class="flex items-center space-x-2") {
+                                                            span(class="font-medium text-gray-600") { (param.key.clone()) }
+                                                            span(class="text-gray-400") { "=" }
+                                                            span(class="text-gray-800") { (param.val.clone()) }
+                                                        }
+                                                        button(
+                                                            class="text-gray-400 hover:text-red-500 hover:bg-red-50 rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity",
+                                                            on:click=move |_| {
+                                                                let mut current = params.get_clone();
+                                                                current.retain(|p| p.key != key_to_remove);
+                                                                params.set(current);
+                                                            }
+                                                        ) {
+                                                            "remove"
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        )
+                                    }
                             },
                             1 => view! {
                                 div{ 
@@ -175,6 +237,7 @@ fn RequestComponent() -> View {
                     }
                 }
             }
+
             // response area
             div(class="flex flex-col p-2") {
                 (if result_show.get() {
